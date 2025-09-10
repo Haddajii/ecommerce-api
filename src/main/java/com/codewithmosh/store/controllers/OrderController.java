b17fcdb94fbfa1870a67df3cf80b18e1953361de
Haddajii <mohamedhaddaji68@gmail.com>
2025-09-10 22:56:00 +0100

    added a method to get a specific order

--- src/main/java/com/codewithmosh/store/controllers/OrderController.java
+++ src/main/java/com/codewithmosh/store/controllers/OrderController.java
@@ -1,11 +1,13 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.OrderDto;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +20,20 @@ public class OrderController {
     @GetMapping
     public List<OrderDto> getAllOrders() {
         return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{orderId}")
+    public OrderDto getOrder(@PathVariable("orderId") Long orderId) {
+        return orderService.getOrder(orderId) ;
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Void> handleException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Error> handleAccessDenied() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error("Access denied"));
     }
 }
--- src/main/java/com/codewithmosh/store/services/OrderService.java
+++ src/main/java/com/codewithmosh/store/services/OrderService.java
@@ -3,9 +3,11 @@ package com.codewithmosh.store.services;
 import com.codewithmosh.store.dtos.OrderDto;
 import com.codewithmosh.store.entities.Order;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.mappers.OrderMapper;
 import com.codewithmosh.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,5 +22,14 @@ public class OrderService {
         var orders = orderRepository.findAllByCustomer(user);
 
         return orders.stream().map(orderMapper::toDto).toList() ;
+    }
+
+    public OrderDto getOrder(long orderId) {
+        var order = orderRepository.getOrderWithItems(orderId).orElseThrow(OrderNotFoundException::new) ;
+        var user = authService.getCurrentUser() ;
+        if(!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("Access denied") ;
+        }
+        return orderMapper.toDto(order) ;
     }
 }
