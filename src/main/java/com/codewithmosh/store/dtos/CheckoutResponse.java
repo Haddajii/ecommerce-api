672759363d63619c6d9adf7b9729e3e13e63b00e
Haddajii <mohamedhaddaji68@gmail.com>
2025-09-12 22:39:48 +0100

    integrate Stripe checkout session with order line items

--- src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
@@ -3,8 +3,14 @@ package com.codewithmosh.store.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data
 public class CheckoutResponse {
     private Long orderId ;
+    private String checkoutUrl ;
+
+    public CheckoutResponse(Long orderId, String checkoutUrl) {
+        this.orderId = orderId;
+        this.checkoutUrl = checkoutUrl;
+
+    }
 }
--- src/main/java/com/codewithmosh/store/services/CheckoutService.java
+++ src/main/java/com/codewithmosh/store/services/CheckoutService.java
@@ -7,21 +7,28 @@ import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.repositories.CartRepository;
 import com.codewithmosh.store.repositories.OrderRepository;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class CheckoutService {
     private final CartRepository cartRepository;
     public final AuthService authService;
     public final OrderRepository orderRepository;
     public final CartService cartService;
+    @Value("${websiteUrl}")
+    private String websiteUrl ;
 
-    public CheckoutResponse checkout(CheckoutRequest request) {
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
 
         if (cart == null) {
@@ -35,9 +42,35 @@ public class CheckoutService {
         var order = Order.fromCart(cart,authService.getCurrentUser());
 
         orderRepository.save(order);
+        //create checkout session
+        var builder = SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                                .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                                        .setCancelUrl(websiteUrl + "/checkout-cancel") ;
+
+
+        order.getItems().forEach(item -> {
+            var LineItem = SessionCreateParams.LineItem.builder()
+                    .setQuantity(Long.valueOf(item.getQuantity()))
+                    .setPriceData(
+                            SessionCreateParams.LineItem.PriceData.builder()
+                                    .setCurrency("USD")
+                                    .setUnitAmountDecimal(item.getUnitPrice())
+                                    .setProductData(
+                                    SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                            .setName(item.getProduct().getName())
+                                            .build()
+                                    ).build()
+                    ).build();
+            builder.addLineItem(LineItem) ;
+        }) ;
+
+        var session = Session.create(builder.build()) ;
+
         cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId()) ;
+
+        return new CheckoutResponse(order.getId() , session.getUrl()) ;
     }
 }
 
